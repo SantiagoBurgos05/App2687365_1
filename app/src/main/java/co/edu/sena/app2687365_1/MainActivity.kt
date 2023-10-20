ff39602a3b253754762e87b4ceff98627d2b112f
Mariale051 <128168046+MiguelQuiroga2412@users.noreply.github.com>
2023-10-20 10:26:15 -0500

    "Kotilin"

--- app/src/main/java/co/edu/sena/app2687365_1/MainActivity.kt
+++ app/src/main/java/co/edu/sena/app2687365_1/MainActivity.kt
@@ -3,44 +3,33 @@ package co.edu.sena.app2687365_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import co.edu.sena.app2687365_1.ui.theme.App2687365_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App2687365_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }// setContent
+    }//onCreate
+}// MainActivity
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    App2687365_1Theme {
-        Greeting("Android")
-    }
+fun MessageCardPreview() {
+    MessageCard(msg = Message("Colleague", "Hey take a look at jetpack compose"))
 }
+
